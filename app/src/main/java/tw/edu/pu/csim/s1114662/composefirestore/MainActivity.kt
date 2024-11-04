9937e46ec0488ae7bfbfb3ece8572f2cc81fa5d4
una2b <syunna2004625@gmail.com>
2024-11-04 15:50:34 +0800

    v5_資料庫按鈕

--- app/src/main/java/tw/edu/pu/csim/s1114662/composefirestore/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1114662/composefirestore/MainActivity.kt
@@ -54,6 +54,7 @@ fun Birth(m: Modifier){
     var userName by remember { mutableStateOf("毛姿云")}
     var userWeight by remember { mutableStateOf(3800)}
     var userPassword by remember { mutableStateOf("")}
+    var msg by remember { mutableStateOf("訊息")}
 
     Column {
         TextField(
@@ -97,6 +98,19 @@ fun Birth(m: Modifier){
 
         Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克"
                 + "\n密碼：$userPassword")
+
+        Row {
+            Button(onClick = {  }) {
+                Text("新增/修改資料")
+            }
+            Button(onClick = {  }) {
+                Text("查詢資料")
+            }
+            Button(onClick = {  }) {
+                Text("刪除資料")
+            }
+        }
+        Text(text = msg)
     }
 
 
